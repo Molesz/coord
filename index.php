c3036d8a07958cd20e53d229b727aeebba022caf
Molesz <molesz1986@gmail.com>
2016-05-02 08:19:20 +0200

    Session kezelés megoldva. Ha nincs session érték, akkor átdob a belépés oldalra.

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
-
-
-
-
 <?php
+session_start();
 
+/// Session ellenrzése. Ha nincs, akkor átdob a belépés oldalra.
+if(isset( $_SESSION['user'] ))
+{
+ /// Ha ba van lépve...
+}
+ else {
+    header('Location: belepes.php');   
+}
 
+echo 'index';
 
 ?>
 
