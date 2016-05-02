93bf308f2508207825c2bc2c2a0579edf85fd6de
Molesz <molesz1986@gmail.com>
2016-05-02 09:03:20 +0200

    Csatlakoztatva az adatbázishoz.

--- config.php
+++ config.php
@@ -1,4 +1,10 @@
 <?php
+$database_host = 'localhost';
+$database_username = 'root';
+$database_password = 'test';
+$database_name = 'coord';
 
-
+$link = mysqli_connect($database_host, $database_username, $database_password,$database_name);
+if (mysqli_connect_errno())
+    die('Could not connect: ' . mysqli_connect_error());
 ?>
